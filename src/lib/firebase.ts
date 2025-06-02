df6d11f09a28a1c356e2e4f694cfc20a7d05908e
_redwan <aminmredwan@gmail.com>
2025-06-03 00:04:56 +0600

    Updated firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,14 +1,21 @@
-import { initializeApp } from 'firebase/app';
-import { getFirestore } from 'firebase/firestore';
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyBemAuu829yoEvKmaIvsBf67VSdzCGdmXI",
+  authDomain: "prevarchform.firebaseapp.com",
+  projectId: "prevarchform",
+  storageBucket: "prevarchform.firebasestorage.app",
+  messagingSenderId: "493026728311",
+  appId: "1:493026728311:web:92379d97aeacaa7834e972",
+  measurementId: "G-4TJXH89ZTC"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore(app);
+const analytics = getAnalytics(app);
